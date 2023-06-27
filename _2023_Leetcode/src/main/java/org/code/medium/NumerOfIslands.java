2511670be0734be2792194be81a3bbf7c6cd5689
Sunil <sunil016@yahoo.com>
2023-06-27 09:51:26 +0530

    #nnnn, number of islands

---
+++ _2023_Leetcode/src/main/java/org/code/medium/NumerOfIslands.java
@@ -0,0 +1,35 @@
+package org.code.medium;
+
+public class NumerOfIslands {
+
+    public static void main(String[] args) {
+        System.out.println(numIslands(new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '1', '0'}
+        }));
+    }
+
+    public static int numIslands(char[][] grid) {
+        int totalIsland = 0;
+        for (int x = 0; x < grid.length; x++)
+            for (int y = 0; y < grid[0].length; y++)
+                if (grid[x][y] == '1') {
+                    df(grid, x, y);
+                    totalIsland++;
+                }
+
+        return totalIsland;
+    }
+
+    private static void df(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) return;
+        if (grid[x][y] == '0') return;
+        grid[x][y] = '0';
+        df(grid, x + 1, y);
+        df(grid, x, y + 1);
+        df(grid, x - 1, y);
+        df(grid, x, y - 1);
+    }
+}
