4a0e1c0bc35dd72117fd0ec1782712edf79d7fb3
Sunil <sunil016@yahoo.com>
2023-06-27 10:08:10 +0530

    #nnnn, level order traveral for tree

---
+++ _2023_Leetcode/src/main/java/org/code/medium/BinaryTreeLevelTraversal.java
@@ -0,0 +1,53 @@
+package org.code.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(levelOrder(root));
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        List<List<Integer>> levels = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int currentSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+            while (currentSize-- > 0) {
+                TreeNode getNode = queue.poll();
+                level.add(getNode.val);
+                if (getNode.left != null) queue.add(getNode.left);
+                if (getNode.right != null) queue.add(getNode.right);
+            }
+            levels.add(level);
+        }
+
+        return levels;
+    }
+}
