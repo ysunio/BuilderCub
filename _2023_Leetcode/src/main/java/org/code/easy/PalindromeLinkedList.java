07645e468f7a7fdce3b29ba9385abf0b9cf18e8e
Sunil <sunil016@yahoo.com>
2023-06-27 08:07:07 +0530

    #nnnn, palindrome linked list

---
+++ _2023_Leetcode/src/main/java/org/code/easy/PalindromeLinkedList.java
@@ -0,0 +1,65 @@
+package org.code.easy;
+
+public class PalindromeLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        void display() {
+            System.out.print(" >> " + this.val);
+            if (this.next != null) this.next.display();
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(10);
+        head.next = new ListNode(20);
+        head.next.next = new ListNode(30);
+        head.next.next.next = new ListNode(20);
+        head.next.next.next.next = new ListNode(10);
+
+        System.out.println(isPalindrome(head));
+    }
+    public static boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+
+        ListNode midNode = findMid(head);
+        ListNode left = head;
+        ListNode right = reverse(midNode);
+        while(left != null && right != null){
+            if(left.val != right.val) return false;
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    private static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+
+    private static ListNode findMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(prev != null) prev.next = null;
+        return slow;
+    }
+}
