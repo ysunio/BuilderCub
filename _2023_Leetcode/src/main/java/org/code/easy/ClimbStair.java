b364c6e47eabdb6db0c560ab0c555d7516084a62
Sunil <sunil016@yahoo.com>
2023-06-29 07:47:04 +0530

    #nnnn climb stair - dp

--- _2023_Leetcode/src/main/java/org/code/easy/ClimbStair.java
+++ _2023_Leetcode/src/main/java/org/code/easy/ClimbStair.java
@@ -4,11 +4,31 @@ public class ClimbStair {
 
     public static void main(String[] args) {
         System.out.println(climbStairs(10));
+        System.out.println(climbStairsDp(10));
     }
 
     public static int climbStairs(int n) {
         int[] mem = new int[n + 1];
         return trav(n, 0, mem);
+    }
+
+    public static int climbStairsDp(int n) {
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] + dp[i + 2];
+        }
+        return dp[0];
+    }
+
+
+    private int trav(int n, int[] mem) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        if (mem[n] != 0) return mem[n];
+        return mem[n] = trav(n - 1, mem) + trav(n - 2, mem);
     }
 
     private static int trav(int n, int idx, int[] mem) {
