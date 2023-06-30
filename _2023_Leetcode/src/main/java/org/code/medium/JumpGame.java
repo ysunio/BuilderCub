917d2d7720c314b62bf0665c2c78dd23ddeeef2b
Sunil <sunil016@yahoo.com>
2023-06-30 08:53:39 +0530

    #nnnn jump game dp

--- _2023_Leetcode/src/main/java/org/code/medium/JumpGame.java
+++ _2023_Leetcode/src/main/java/org/code/medium/JumpGame.java
@@ -4,6 +4,23 @@ public class JumpGame {
 
     public static void main(String[] args) {
         System.out.println(canJump(new int[]{2, 4, 5, 3, 2, 1}));
+        System.out.println(canJumpDP(new int[]{2, 4, 5, 3, 2, 1}));
+    }
+
+    public static boolean canJumpDP(int[] nums) {
+        int len = nums.length;
+        if(len <= 1) return true;
+
+        int elementToReachIdx = len - 1;
+        while(true){
+            int preItr = elementToReachIdx - 1;
+            int lastElementReached = elementToReachIdx;
+            while(preItr >= 0 && elementToReachIdx - preItr <= nums[preItr]) lastElementReached = preItr--;
+
+            if(lastElementReached == 0) return true;
+            if(elementToReachIdx == lastElementReached) return false;
+            elementToReachIdx = lastElementReached;
+        }
     }
 
     public static boolean canJump(int[] nums) {
