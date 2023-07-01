019041b631e4e10add0c1ea3ade68ea99fdcf3cd
Sunil <sunil016@yahoo.com>
2023-07-01 11:55:50 +0530

    #nnnn jump game II

---
+++ _2023_Leetcode/src/main/java/org/code/medium/JumpGameII.java
@@ -0,0 +1,25 @@
+package org.code.medium;
+
+public class JumpGameII {
+    public static void main(String[] args) {
+        System.out.println(jump(new int[]{2, 3, 1, 1, 4}));
+    }
+
+    public static int jump(int[] nums) {
+        int minCost = 0;
+        int leastElement = nums.length - 1;
+
+        while (leastElement > 0) {
+            int idx = leastElement - 1;
+            int lastElement = idx - 1;
+            while (idx >= 0) {
+                if (leastElement - idx <= nums[idx]) lastElement = idx;
+                idx--;
+            }
+            if (leastElement == lastElement) return -1;
+            leastElement = lastElement;
+            minCost++;
+        }
+        return minCost;
+    }
+}
