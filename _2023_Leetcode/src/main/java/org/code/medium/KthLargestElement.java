0e4ba9ab612c7c7ed63d55c75f451cccf2db9aa5
Sunil <sunil016@yahoo.com>
2023-07-01 19:02:35 +0530

    #nnnn kth largest element

--- _2023_Leetcode/src/main/java/org/code/medium/KthLargestElement.java
+++ _2023_Leetcode/src/main/java/org/code/medium/KthLargestElement.java
@@ -1,2 +1,18 @@
-package org.code.medium;public class KthLargestElement {
+package org.code.medium;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3, 2, 4, 5, 6}, 2));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        for (int x : nums) queue.add(x);
+        int val = -1;
+        for (int idx = 0; idx < k; idx++)
+            val = queue.remove();
+        return val;
+    }
 }
