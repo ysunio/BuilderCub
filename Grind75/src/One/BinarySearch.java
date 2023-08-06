d8126645e8b0d84d49dad353e46543ec7bd3a559
Sunil <sunil016@yahoo.com>
2023-08-06 18:42:56 +0530

    grind75 - valid anagram

---
+++ Grind75/src/One/BinarySearch.java
@@ -0,0 +1,2 @@
+package One;public class BinarySearch {
+}
---
+++ Grind75/src/One/ValidAnagram.java
@@ -0,0 +1,13 @@
+package One;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        int[] charStore = new int[26];
+        for(char c : s.toCharArray())
+            charStore[c-'a']++;
+        for(char c : t.toCharArray())
+            charStore[c-'a']--;
+        for(int c : charStore) if(c != 0) return false;
+        return true;
+    }
+}
