680e02a00c7d1459e4356e7d6cbeb2f4ab803803
Sunil <sunil016@yahoo.com>
2023-08-06 18:21:42 +0530

    grind75 - invert binary tree

---
+++ Grind75/src/One/InvertTree.java
@@ -0,0 +1,25 @@
+package One;
+
+public class InvertTree {
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        invert(root);
+        return root;
+    }
+
+    private void invert(TreeNode root){
+        if(root == null) return;
+        TreeNode leftNode = root.left;
+        root.left = root.right;
+        root.right = leftNode;
+        invert(root.left);
+        invert(root.right);
+    }
+}
