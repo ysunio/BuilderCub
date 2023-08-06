d7efa66f1ceb257375f04887bd878a778ab64939
Sunil <sunil016@yahoo.com>
2023-08-06 19:08:02 +0530

    grind75 - flood fill

---
+++ Grind75/src/One/FloodFills.java
@@ -0,0 +1,29 @@
+package One;
+
+public class FloodFills {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        track(image, sr, sc, newColor, image[sr][sc]);
+        return image;
+    }
+
+
+    public void track(int[][] image, int sr, int sc, int newColor,int oldColor) {
+        if(sr<0 || sc<0 || sr>=image.length || sc >= image[0].length)
+            return;
+
+        if(image[sr][sc]==newColor){
+            return;
+        }
+
+        if(image[sr][sc]!=oldColor)
+            return;
+
+        image[sr][sc] = newColor;
+
+        track(image,sr,sc+1,newColor,oldColor);
+        track(image,sr+1,sc,newColor,oldColor);
+        track(image,sr,sc-1,newColor,oldColor);
+        track(image,sr-1,sc,newColor,oldColor);
+    }
+}
