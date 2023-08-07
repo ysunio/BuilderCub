e17ed9287e8aef677ebd082fd1e1f2c5f4f14303
Sunil <sunil016@yahoo.com>
2023-08-07 16:25:06 +0530

    grind75 - linked list cycle

---
+++ Grind75/src/One/LinkedListCycle.java
@@ -0,0 +1,24 @@
+package One;
+
+public class LinkedListCycle {
+
+    class ListNode{
+        int val;
+        ListNode next;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(
+                fast != null &&
+                        fast.next != null &&
+                        fast != slow){
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast == slow;
+    }
+}
