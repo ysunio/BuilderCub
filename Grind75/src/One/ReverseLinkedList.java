fcf757c464c1703815048ec91a8322f090c59195
Sunil <sunil016@yahoo.com>
2023-08-07 16:29:36 +0530

    grind75 - reverse linked list

---
+++ Grind75/src/One/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package One;
+
+public class ReverseLinkedList {
+    class ListNode{
+        int val;
+        ListNode next;
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
