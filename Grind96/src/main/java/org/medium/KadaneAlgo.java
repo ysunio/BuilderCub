229bd290478f5f7b24218f88ad2ef957f7295f24
Sunil <sunil016@yahoo.com>
2023-08-26 22:33:58 +0530

    add test

--- Grind96/src/main/java/org/medium/KadaneAlgo.java
+++ Grind96/src/main/java/org/medium/KadaneAlgo.java
@@ -1,12 +1,7 @@
 package org.medium;
 
 public class KadaneAlgo {
-
-    public static void main(String[] args) {
-        System.out.println(maxSubArray(new int[]{-1, -2, 4, 5, 6, 4, 3, 2, 4,}));
-    }
-
-    static int maxSubArray(int[] nums) {
+    int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
         int tSum = 0;
         for (int num : nums) {
