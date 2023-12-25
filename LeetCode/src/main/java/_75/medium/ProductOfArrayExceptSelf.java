272a4d15d442812838aaf96d7e56b2370105297d
Sunil <sunil016@yahoo.com>
2023-12-25 19:48:56 +0530

    238, product of array expect self

---
+++ LeetCode/src/main/java/_75/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,33 @@
+package _75.medium;
+
+public class ProductOfArrayExceptSelf {
+        public int[] productExceptSelf(int[] nums) {
+            int[] lMul = new int[nums.length];
+            int[] rMul = new int[nums.length];
+
+            // Base Case
+            lMul[0] = 1;
+            rMul[rMul.length - 1] = 1;
+
+            // Left
+            for(int idx = 1; idx < nums.length; idx++){
+                lMul[idx] = lMul[idx - 1] * nums[idx - 1];
+            }
+
+            // Right
+            for(int idx = nums.length - 2; idx >= 0; idx--){
+                rMul[idx] = rMul[idx + 1] * nums[idx + 1];
+            }
+
+            // Mul = Left * Right
+            for(int idx = 1; idx < nums.length - 1; idx++){
+                nums[idx] = lMul[idx] * rMul[idx];
+            }
+
+            // Edges
+            nums[0] = rMul[0];
+            nums[nums.length - 1] = lMul[nums.length - 1];
+
+            return nums;
+    }
+}
---
+++ LeetCode/src/test/java/_75/medium/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,17 @@
+package _75.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductOfArrayExceptSelfTest {
+
+    @Test
+    void productExceptSelf() {
+        ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
+        assertArrayEquals(new int[]{24,12,8,6}, productOfArrayExceptSelf.productExceptSelf(new int[]{1,2,3,4}));
+        assertArrayEquals(new int[]{0,0,9,0,0}, productOfArrayExceptSelf.productExceptSelf(new int[]{-1,1,0,-3,3}));
+    }
+}
