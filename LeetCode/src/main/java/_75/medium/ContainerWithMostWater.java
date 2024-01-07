a269426db2d3fefa2a120ea2e3ab3b9e4cec2ef5
Sunil <sunil016@yahoo.com>
2024-01-07 19:41:26 +0530

    11, container with most water

---
+++ LeetCode/src/main/java/_75/medium/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+package _75.medium;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = -1;
+
+        int sIdx = 0;
+        int eIdx = height.length - 1;
+
+        while(sIdx <= eIdx){
+            int cAreal = (eIdx - sIdx) * Math.min(height[sIdx], height[eIdx]);
+            maxArea = Math.max(maxArea, cAreal);
+            if(height[sIdx] > height[eIdx]) eIdx--; else sIdx++;
+        }
+
+        return maxArea;
+    }
+}
