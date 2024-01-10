83aca2e45febaae8b0f0d839bbdc28b937470e66
Sunil <sunil016@yahoo.com>
2024-01-10 08:19:51 +0530

    1207, unique number of occurances

---
+++ LeetCode/src/test/java/_75/easy/UniqueOccurrenceTest.java
@@ -0,0 +1,16 @@
+package _75.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueOccurrenceTest {
+
+    @Test
+    void uniqueOccurrences() {
+        UniqueOccurrence uniqueOccurrence = new UniqueOccurrence();
+        assertFalse(uniqueOccurrence.uniqueOccurrences(new int[]{1000,2,2,-1000,1,3}));
+        assertTrue(uniqueOccurrence.uniqueOccurrences(new int[]{1,2,2,1,1,3}));
+        assertTrue(uniqueOccurrence.uniqueOccurrences(new int[]{-3,0,1,-3,1,1,1,-3,10,0}));
+    }
+}
