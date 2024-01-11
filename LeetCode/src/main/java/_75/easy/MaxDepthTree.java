6dcdfc9ebb56e92d625dafb81b3a409876d9ace0
Sunil <sunil016@yahoo.com>
2024-01-11 19:18:22 +0530

    104, max depth tree

---
+++ LeetCode/src/main/java/_75/easy/MaxDepthTree.java
@@ -0,0 +1,27 @@
+package _75.easy;
+
+public class MaxDepthTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
