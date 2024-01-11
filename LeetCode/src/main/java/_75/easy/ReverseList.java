ff73bbfc23abd8f44cfd91784871ced7845c91e2
Sunil <sunil016@yahoo.com>
2024-01-11 19:30:30 +0530

    206, reverse linked list

---
+++ LeetCode/src/main/java/_75/easy/ReverseList.java
@@ -0,0 +1,34 @@
+package _75.easy;
+
+public class ReverseList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextHead = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextHead;
+        }
+
+        return prev;
+    }
+}
