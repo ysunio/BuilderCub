d76ea9b5cf49139f0dc6ff15730a5605bd3f19f0
Sunil <sunil016@yahoo.com>
2024-01-11 19:50:28 +0530

    2095, delete middle node

---
+++ LeetCode/src/test/java/_75/medium/DeleteMiddleNodeTest.java
@@ -0,0 +1,30 @@
+package _75.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteMiddleNodeTest {
+
+    @Test
+    void deleteMiddle() {
+        DeleteMiddleNode.ListNode head = new DeleteMiddleNode.ListNode(10);
+        head.next = new DeleteMiddleNode.ListNode(20);
+        head.next.next = new DeleteMiddleNode.ListNode(30);
+        head.next.next.next = new DeleteMiddleNode.ListNode(40);
+        head.next.next.next.next = new DeleteMiddleNode.ListNode(50);
+
+        DeleteMiddleNode.ListNode deleteMiddle = new DeleteMiddleNode().deleteMiddle(head);
+
+        List<Integer> nodeListArray = new ArrayList<>();
+        while (deleteMiddle != null) {
+            nodeListArray.add(deleteMiddle.val);
+            deleteMiddle = deleteMiddle.next;
+        }
+
+        assertArrayEquals(new Integer[]{10, 20, 40, 50}, nodeListArray.toArray());
+    }
+}
