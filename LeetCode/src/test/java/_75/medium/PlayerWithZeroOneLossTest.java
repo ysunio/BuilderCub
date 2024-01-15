ccde778f9d36ef912e693bb8d736bcf9d39af884
Sunil <sunil016@yahoo.com>
2024-01-15 17:12:49 +0530

    2225, find players with zero or one losses

---
+++ LeetCode/src/test/java/_75/medium/PlayerWithZeroOneLossTest.java
@@ -0,0 +1,18 @@
+package _75.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerWithZeroOneLossTest {
+
+    @Test
+    void findWinners() {
+        PlayerWithZeroOneLoss playerWithZeroOneLoss = new PlayerWithZeroOneLoss();
+        List<List<Integer>> aResult = playerWithZeroOneLoss.findWinners(new int[][]{{2,3},{1,3},{5,4},{6,4}});
+        assertEquals(List.of(1,2,5,6), aResult.get(0));
+        assertEquals(List.of(), aResult.get(1));
+    }
+}
