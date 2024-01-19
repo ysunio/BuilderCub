0d92f10186c5041fced5f7027963d74fec4cb126
Sunil <sunil016@yahoo.com>
2024-01-19 07:27:48 +0530

    931, max falling path sum

---
+++ LeetCode/src/test/java/_75/medium/MinFallingPathTest.java
@@ -0,0 +1,15 @@
+package _75.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinFallingPathTest {
+
+    @Test
+    void minFallingPathSum() {
+        int[][] pathFalling = new int[][]{{2,1,3},{6,5,4},{7,8,9}};
+        int minPathSum = new MinFallingPath().minFallingPathSum(pathFalling);
+        assertEquals(13, minPathSum);
+    }
+}
