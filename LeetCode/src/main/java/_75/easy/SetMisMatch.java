096b1e8b55dde42ab37100f5271d079a8f94ecfd
Sunil <sunil016@yahoo.com>
2024-01-22 11:39:40 +0530

    645, Set Mismatch

---
+++ LeetCode/src/main/java/_75/easy/SetMisMatch.java
@@ -0,0 +1,24 @@
+package _75.easy;
+
+public class SetMisMatch {
+    public int[] findErrorNums(int[] nums) {
+        int totalSumNeeds = 0;
+        int totalSum = 0;
+        int duplicateNumber = 0;
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (nums[Math.abs(nums[idx]) - 1] < 0) {
+                duplicateNumber = Math.abs(nums[idx]);
+                break;
+            }
+            nums[Math.abs(nums[idx]) - 1] *= -1;
+        }
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            totalSumNeeds += idx + 1;
+            totalSum += Math.abs(nums[idx]);
+        }
+
+        return new int[]{duplicateNumber, totalSumNeeds - (totalSum - duplicateNumber)};
+    }
+}
---
+++ LeetCode/src/test/java/_75/easy/SetMisMatchTest.java
@@ -0,0 +1,15 @@
+package _75.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetMisMatchTest {
+
+    @Test
+    void findErrorNums() {
+        SetMisMatch setMisMatch = new SetMisMatch();
+        assertArrayEquals(new int[]{2, 1}, setMisMatch.findErrorNums(new int[]{2, 2, 3}));
+        assertArrayEquals(new int[]{1, 2}, setMisMatch.findErrorNums(new int[]{1, 1, 3}));
+    }
+}
