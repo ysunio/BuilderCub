1492a3d074c3a8d8a8cd6528b1068fcfa566f494
Sunil <sunil016@yahoo.com>
2024-01-23 21:45:19 +0530

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ LeetCode/src/test/java/_75/medium/MaxLenSubsequenceUniqueCharTest.java
@@ -0,0 +1,17 @@
+package _75.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxLenSubsequenceUniqueCharTest {
+
+    @Test
+    void maxLength() {
+        MaxLenSubsequenceUniqueChar max = new MaxLenSubsequenceUniqueChar();
+        assertEquals(4, max.maxLength(List.of("un","iq","ue")));
+        assertEquals(6, max.maxLength(List.of("cha","r","act","ers")));
+    }
+}
