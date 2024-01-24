578346902ade643d195bf2d4f5cf705ae8ce993d
Sunil <sunil016@yahoo.com>
2024-01-24 15:28:37 +0530

    1457. Pseudo-Palindromic Paths in a Binary Tree

---
+++ LeetCode/src/test/java/_75/medium/Pseudo_Palindromic_BTTest.java
@@ -0,0 +1,17 @@
+package _75.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Pseudo_Palindromic_BTTest {
+
+    @Test
+    void pseudoPalindromicPaths() {
+        Pseudo_Palindromic_BT.TreeNode root = new Pseudo_Palindromic_BT.TreeNode(1);
+        root.left = new Pseudo_Palindromic_BT.TreeNode(2);
+        root.right = new Pseudo_Palindromic_BT.TreeNode(1);
+        root.right.left = new Pseudo_Palindromic_BT.TreeNode(1);
+        assertEquals(1, Pseudo_Palindromic_BT.pseudoPalindromicPaths(root));
+    }
+}
