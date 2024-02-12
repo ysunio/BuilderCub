fcaaaea35f09531f23e95644bb335b9cd382c4d7
Sunil <sunil016@yahoo.com>
2024-02-12 09:05:58 +0530

    169. Majority Element

---
+++ LeetCode/src/main/java/_75/easy/MajorityElement.java
@@ -0,0 +1,16 @@
+package _75.easy;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int majorityElement = -1;
+        int majorityElementCount = 0;
+
+        for (int num : nums) {
+            if (majorityElementCount == 0) majorityElement = num;
+            if (majorityElement == num) majorityElementCount++;
+            else majorityElementCount--;
+        }
+
+        return majorityElement;
+    }
+}
---
+++ LeetCode/src/test/java/_75/easy/MajorityElementTest.java
@@ -0,0 +1,14 @@
+package _75.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MajorityElementTest {
+
+    @Test
+    void majorityElement() {
+        MajorityElement majorityElement = new MajorityElement();
+        assertEquals(2, majorityElement.majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2}));
+    }
+}
