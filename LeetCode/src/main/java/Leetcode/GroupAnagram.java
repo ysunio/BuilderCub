8c8b83135bb08594032e50a71af871b0f51ed52f
Sunil <sunil016@yahoo.com>
2024-03-31 12:31:53 +0530

    GroupAnagram

---
+++ LeetCode/src/main/java/Leetcode/GroupAnagram.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+import java.util.*;
+
+public class GroupAnagram {
+
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+
+    static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> mP = new HashMap<>();
+
+        for(String str : strs){
+            char[] rChar = str.toCharArray();
+            Arrays.sort(rChar);
+            String key = String.valueOf(rChar);
+
+            if(!mP.containsKey(key)) mP.put(key, new ArrayList<>());
+            mP.get(key).add(str);
+        }
+
+        return new ArrayList<>(mP.values());
+    }
+}
