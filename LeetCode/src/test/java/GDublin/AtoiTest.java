df7709378a1ae9af57da2e80a1993f5b50c13d05
Sunil <sunil016@yahoo.com>
2024-08-04 09:18:53 +0100

    - String -> Number : Atoi

---
+++ LeetCode/src/test/java/GDublin/AtoiTest.java
@@ -0,0 +1,17 @@
+package GDublin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AtoiTest {
+
+    @Test
+    void myAtoi() {
+        Atoi atoi = new Atoi();
+        Assertions.assertEquals(10, atoi.myAtoi("10"));
+        Assertions.assertEquals(-10, atoi.myAtoi("-10abc"));
+        Assertions.assertEquals(-42, atoi.myAtoi("  -042"));
+    }
+}
